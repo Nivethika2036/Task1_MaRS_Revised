4a4d5536a04af7727f53e9e0c7015e8431503fda
Nivethika2036 <ec25b1106@iiitdm.ac.in>
2026-04-10 21:06:25 +0530

    Create Blinking_LED.ino
    Tinkercad Link :
    https://www.tinkercad.com/things/h5eNgWOrLZm-fabulous-lahdi/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard&sharecode=3vOlg25GGmIMMtGK_t6Vy-ZrSWnkyKPplViXrTJNoqk2

---
+++ Blinking_LED.ino
@@ -0,0 +1,51 @@
+// Define LED pins
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+
+// Variables to store time
+unsigned long previousMillis1 = 0;
+unsigned long previousMillis2 = 0;
+unsigned long previousMillis3 = 0;
+
+// Intervals
+int interval1 = 500;
+int interval2 = 1000;
+int interval3 = 1500;
+
+// LED states
+bool state1 = LOW;
+bool state2 = LOW;
+bool state3 = LOW;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  // LED 1 (500 ms)
+  if (currentMillis - previousMillis1 >= interval1) {
+    previousMillis1 = currentMillis;
+    state1 = !state1;
+    digitalWrite(led1, state1);
+  }
+
+  // LED 2 (1000 ms)
+  if (currentMillis - previousMillis2 >= interval2) {
+    previousMillis2 = currentMillis;
+    state2 = !state2;
+    digitalWrite(led2, state2);
+  }
+
+  // LED 3 (1500 ms)
+  if (currentMillis - previousMillis3 >= interval3) {
+    previousMillis3 = currentMillis;
+    state3 = !state3;
+    digitalWrite(led3, state3);
+  }
+}
+
